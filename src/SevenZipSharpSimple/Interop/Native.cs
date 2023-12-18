50b41e0398d20c89375074544b4154be0c620393
Marius Keuck <mkeuck@exocad.com>
2023-12-18 15:16:59 +0100

    The LoadLibrary method now adds the directory of the executable to the given library name, if the first attempt to load the native library failed.

--- src/SevenZipSharpSimple/Interop/Native.cs
+++ src/SevenZipSharpSimple/Interop/Native.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 namespace SevenZipSharpSimple.Interop
 {
     /// <summary>
-    /// Native method declarations.
+    /// Native library access.
     /// </summary>
     internal static class Native
     {
@@ -15,27 +15,53 @@ namespace SevenZipSharpSimple.Interop
         /// </summary>
         /// <param name="classId">The UUID of the class to create.</param>
         /// <param name="interfaceId">The UUID of the interface to create.</param>
-        /// <param name="interface">The created interface or <c>null</c>, if the creation failed.</param>
+        /// <param name="interfacePtr">The created interface or <c>null</c>, if the creation failed.</param>
         /// <returns>Zero (0), if the operation succeeded or a non-zero value indicating an error otherwise.</returns>
         [UnmanagedFunctionPointer(CallingConvention.StdCall)]
         public delegate int CreateObjectDelegate(
             [In] ref Guid classId,
             [In] ref Guid interfaceId,
 #if NET8_0_OR_GREATER
-            out IntPtr @interfacePtr
+            out IntPtr interfacePtr
 #else
-            [System.Runtime.InteropServices.MarshalAs(UnmanagedType.Interface)] out object @interface
+            [System.Runtime.InteropServices.MarshalAs(UnmanagedType.Interface)] out object interfacePtr
 #endif
             );
 
+        /// <summary>
+        /// Loads a native library. If the first attempt failed, the library is searched
+        /// in the directory of the current executable.
+        /// </summary>
+        /// <param name="name">The name or path of the library to load.</param>
+        /// <returns>A pointer to the library handle.</returns>
         public static IntPtr LoadLibrary(string name)
         {
-            var path = System.IO.Path.Join(System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), name);
-            var handle = NativeLibrary.Load(path);
+            if (NativeLibrary.TryLoad(name, out var handle))
+            {
+                return handle;
+            }
 
-            return handle;
+            if (!System.IO.Path.IsPathRooted(name))
+            {
+                var baseDirectory = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location);
+                var path = System.IO.Path.Join(baseDirectory, name);
+
+                if (!string.IsNullOrEmpty(baseDirectory) && NativeLibrary.TryLoad(path, out handle))
+                {
+                    return handle;
+                }
+            }
+
+            return IntPtr.Zero;
         }
 
+        /// <summary>
+        /// Loads a function named <paramref name="name"/> from the given <paramref name="library"/>.
+        /// </summary>
+        /// <typeparam name="TDelegate">The delegate declaring the function signature.</typeparam>
+        /// <param name="library">The library containing the function to load.</param>
+        /// <param name="name">The name of the function to load.</param>
+        /// <returns>A delegate for the loaded native function.</returns>
         public static TDelegate GetExportAs<TDelegate>(IntPtr library, string name)
             where TDelegate : class
         {
